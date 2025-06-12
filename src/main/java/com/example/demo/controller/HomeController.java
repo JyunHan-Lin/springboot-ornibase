7231b93711a620f1136d8607ef5cf5d8d9436752
JyunHan-Lin <sabettle0331@gmail.com>
2025-06-12 15:56:19 +0800

    11406121556

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.dto.UserCert;
+import com.example.demo.service.DiscussService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/preview")
+public class HomeController {
+
+	@Autowired
+	private DiscussService discussService;
+	
+	@GetMapping
+	public String mainPage(Model model, String youtubeVideoId, HttpSession session) {
+	    UserCert userCert = (UserCert) session.getAttribute("userCert");
+	    Integer userId = userCert.getUserId();
+	    model.addAttribute("discussList", discussService.getDiscussByUserId(userId));
+		return "main";
+	}
+}
